881e60ca317ca80a1c285e49ddd000cd420f1d3d
vishalthakur <vishalthakur970820@gmail.com>
2025-07-30 15:30:31 +0530

    Added info routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 const bookingRoutes = require('./booking')
+const {InfoControlelr} = require('../../controllers')
 
+router.get('/info', InfoControlelr.info)
 router.use('/bookings', bookingRoutes)
 
 module.exports = router
